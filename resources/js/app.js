8175f1f4fe61f5e84af9006759ff3cffc30e283d
ericserka <ericserka@hotmail.com>
2023-04-23 21:21:12 -0300

    feat: alpine js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-import './bootstrap';
+import "./bootstrap";
+
+import mask from "@alpinejs/mask";
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.plugin(mask);
+
+Alpine.start();
